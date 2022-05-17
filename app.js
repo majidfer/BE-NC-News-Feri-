8b5cb1826692bffcb1cb20a7d49c6679fb3ff408
ffm-id <ferialfahri@gmail.com>
2022-05-17 08:07:09 +0100

    Removing console log

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const app = express();
 app.get("/api/topics", getTopics);
 
 // 404 Error - Route not found
-app.get("/", (req, res, next) => {
+app.use("/*", (req, res, next) => {
   res.status(404).send({ message: "Route not found" });
+  next();
 });
 
 // 500 Error
 app.use((err, req, res, next) => {
-  console.log(err);
   res.status(500).send({ message: "Internal server error" });
 });
 
