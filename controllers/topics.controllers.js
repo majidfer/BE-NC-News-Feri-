498f80ba0c0ffb70b3d69f428e839e091b7067aa
ffm-id <ferialfahri@gmail.com>
2022-05-17 08:09:16 +0100

    Changing topics to be sent on an object with a key of topics and a value of the array full of topics

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -3,7 +3,7 @@ const { fetchTopics } = require("../models/topics.models.js");
 exports.getTopics = (req, res, next) => {
   fetchTopics()
     .then((topics) => {
-      res.status(200).send(topics);
+      res.status(200).send({ topics });
     })
     .catch((err) => {
       next(err);
