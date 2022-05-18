f570b158ea2c3c72ce2227a6764d5770fc5a99a1
ffm-id <ferialfahri@gmail.com>
2022-05-18 10:42:44 +0100

    Adding model for PATCH /api/article/:article_id

--- models/articles.models.js
+++ models/articles.models.js
@@ -2,14 +2,25 @@ const db = require("../db/connection.js");
 
 exports.fetchArticle = (article_id) => {
   return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      } else
+      return rows[0];
+    });
+};
+
+exports.fetchArticleToPatch = (article_id, inc_votes) => {
+  return db
     .query(
-      "SELECT * FROM articles WHERE article_id = $1;",
-      [article_id]
+      "UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;",
+      [inc_votes, article_id]
     )
     .then(({ rows }) => {
       if (rows.length === 0) {
-          return Promise.reject({status: 404, msg: "Article not found" });
-      }
-        return rows[0];
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      } else
+      return rows[0];
     });
 };
