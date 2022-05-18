ccf029af882935b2ac78305bb2c6d95243c5cdc2
ffm-id <ferialfahri@gmail.com>
2022-05-18 15:48:03 +0100

    Refactoring db query to include total count of comments of the article

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,13 +1,29 @@
 const db = require("../db/connection.js");
 
 exports.fetchArticle = (article_id) => {
+  const queryStr = `
+  SELECT 
+    articles.article_id,
+    articles.title,
+    articles.topic,
+    articles.author,
+    articles.body,
+    articles.created_at,
+    articles.votes,
+    COUNT(comment_id)::int AS total_comments
+  FROM
+    articles
+  LEFT JOIN comments
+  ON articles.article_id = comments.article_id
+  WHERE articles.article_id = $1
+  GROUP BY articles.article_id;`;
+  
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .query(queryStr, [article_id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Article not found" });
-      } else
-      return rows[0];
+      } else return rows[0];
     });
 };
 
@@ -20,7 +36,6 @@ exports.fetchArticleToPatch = (article_id, inc_votes) => {
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Article not found" });
-      } else
-      return rows[0];
+      } else return rows[0];
     });
 };
