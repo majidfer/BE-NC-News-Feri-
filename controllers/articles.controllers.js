47b6c801cd262cc5190a7b0f29e232ea11c800d2
ffm-id <ferialfahri@gmail.com>
2022-05-18 10:41:17 +0100

    Adding controller for PATCH /api/articles/:article_id

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,12 +1,27 @@
-const { fetchArticle } = require("../models/articles.models.js");
+const {
+  fetchArticle,
+  fetchArticleToPatch,
+} = require("../models/articles.models.js");
 
 exports.getArticle = (req, res, next) => {
   const { article_id } = req.params;
   fetchArticle(article_id)
     .then((article) => {
-    res.status(200).send({ article });
-  })
-  .catch((err) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
       next(err);
-  });
+    });
+};
+
+exports.patchArticle = (req, res, next) => {
+  const parsedArticleId = parseInt(req.params.article_id);
+  const { inc_votes } = req.body;
+  fetchArticleToPatch(parsedArticleId, inc_votes)
+    .then((updatedArticle) => {
+      res.status(200).send({ updatedArticle });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
