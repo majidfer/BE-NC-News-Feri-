e2363e5367ff779a1d1ccb75aa43b3fe1348dc25
ffm-id <ferialfahri@gmail.com>
2022-05-18 12:46:54 +0100

    Adding test for ncnews #6 GET /api/users

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -134,6 +134,25 @@ describe("PATCH /api/articles/:article_id", () => {
   });
 });
 
+describe("GET /api/users", () => {
+  test("200: responds with an object with a key of users, and a value of the array full of users.", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.users).toBeInstanceOf(Array);
+        expect(body.users).not.toHaveLength(0);
+        body.users.forEach((user) => {
+          expect(user).toMatchObject({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
+
 describe("Error handling", () => {
   test("404: responds with 404 error when received a request with invalid route(s)", () => {
     return request(app)
