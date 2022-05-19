84602dd9b30f7c71e053390facd1d48f100e6886
ffm-id <ferialfahri@gmail.com>
2022-05-19 10:22:58 +0100

    Adding model for GET /api/articles/:article_id/comments

---
+++ models/comments.models.js
@@ -0,0 +1,22 @@
+const db = require("../db/connection.js");
+
+exports.fetchComments = (article_id) => {
+  const queryStr = `
+    SELECT 
+        comments.comment_id,
+        comments.body,
+        comments.author,
+        comments.votes,
+        comments.created_at
+    FROM articles
+    LEFT JOIN comments
+    ON comments.article_id = articles.article_id
+    WHERE articles.article_id = $1;`;
+  return db.query(queryStr, [article_id]).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "Article not found" });
+    } else if (rows[0].comment_id === null) {
+      return Promise.reject({ status: 404, msg: "Comments not found" });
+    } else return rows;
+  });
+};
