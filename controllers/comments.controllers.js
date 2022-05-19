9300f24c8cfec3d375dd1053699a5fabcddbd62f
ffm-id <ferialfahri@gmail.com>
2022-05-19 12:10:39 +0100

    Adding controller for POST /api/articles/:article_id/comments

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,12 +1,24 @@
-const { fetchComments } = require("../models/comments.models.js");
+const { fetchComments, postComment } = require("../models/comments.models.js");
 
 exports.getComments = (req, res, next) => {
-    const { article_id } = req.params;
-    fetchComments(article_id)
-      .then((comments) => {
-        res.status(200).send({ comments });
-      })
-      .catch((err) => {
-        next(err);
-      });
-  };
+  const { article_id } = req.params;
+  fetchComments(article_id)
+    .then((comments) => {
+      res.status(200).send({ comments });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.addComment = (req, res, next) => {
+  const { article_id } = req.params;
+  const { username, body } = req.body;
+  postComment(article_id, username, body)
+    .then((newComment) => {
+      res.status(201).send({ newComment });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
