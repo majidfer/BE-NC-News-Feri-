30f3ae3b2c59a97b6971b1f78aa6ddad6e928f59
ffm-id <ferialfahri@gmail.com>
2022-05-19 09:20:50 +0100

    Removing body from expected article

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -49,7 +49,6 @@ describe("GET /api/articles", () => {
               title: expect.any(String),
               topic: expect.any(String),
               author: expect.any(String),
-              body: expect.any(String),
               created_at: expect.any(String),
               votes: expect.any(Number),
               comment_count: expect.any(Number),
