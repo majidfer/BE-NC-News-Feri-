7a9d3280fa7629be3c4ee3db6116e0ac4ff19ba7
ffm-id <ferialfahri@gmail.com>
2022-05-20 22:59:43 +0100

    Adding connection config

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,20 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
 
-module.exports = new Pool();
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {};
+
+module.exports = new Pool(config);
