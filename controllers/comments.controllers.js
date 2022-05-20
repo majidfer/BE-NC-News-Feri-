1f5c72fc592b2fdabe054725bdc30e6c3e58bc74
ffm-id <ferialfahri@gmail.com>
2022-05-20 14:57:26 +0100

    Adding controller for DELETE /api/comments/:comment_id

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,4 +1,8 @@
-const { fetchComments, postComment } = require("../models/comments.models.js");
+const {
+  fetchComments,
+  postComment,
+  deleteComment,
+} = require("../models/comments.models.js");
 
 exports.getComments = (req, res, next) => {
   const { article_id } = req.params;
@@ -17,6 +21,17 @@ exports.addComment = (req, res, next) => {
   postComment(article_id, username, body)
     .then((newComment) => {
       res.status(201).send({ newComment });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getCommentToDelete = (req, res, next) => {
+  const { comment_id } = req.params;
+  deleteComment(comment_id)
+    .then((result) => {
+      res.status(204).send({ result });
     })
     .catch((err) => {
       next(err);
